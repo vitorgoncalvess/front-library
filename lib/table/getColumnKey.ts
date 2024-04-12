22eb47345b07c5080eb18314f3a9f10c1d6b902a
vitorgoncalvess <vitor.gsilva@sptech.school>
2024-04-12 14:55:25 -0300

    ajuste tabela

--- lib/table/getColumnKey.ts
+++ lib/table/getColumnKey.ts
@@ -1,5 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getColumnKey = (item: any, colKey: string) => {
+  if (colKey.includes(".")) {
+    const keys = colKey.split(".");
+    let val = item[keys[0]];
+    keys.shift();
+    keys.forEach((key) => {
+      val = val[key];
+    });
+    return val;
+  }
   return item && item[colKey];
 };
 
